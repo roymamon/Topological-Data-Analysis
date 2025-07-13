6d1129d67212aab5cd2977b8336daeefd65fd7c9
Roy Mamon <mamon8521@gmail.com>
2025-07-13 22:30:51 +0300

    visual representation

--- Persistence_Diagram_A.py
+++ Persistence_Diagram_A.py
@@ -7,6 +7,14 @@ points = pd.read_csv("data_A.csv")
 
 diagrams = ripser(points.values)['dgms']
 
+plt.figure(figsize=(6, 6))
+plt.scatter(points["x"], points["y"], s=10)
+plt.title("Point Cloud for data_A.csv")
+plt.axis("equal")
+plt.grid(True)
+plt.savefig("data_A_pointcloud.png", dpi=300)
+plt.show()
+
 fig, ax = plt.subplots(1, 2, figsize=(12, 5))
 plot_diagrams(diagrams[0], ax=ax[0], show=False)
 plot_diagrams(diagrams[1], ax=ax[1], show=False)
