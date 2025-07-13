3f336a3a885dcc23573b7988247071af4134dd4a
Roy Mamon <mamon8521@gmail.com>
2025-07-13 23:06:08 +0300

    data_B

---
+++ Persistence_Diagram_B.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from ripser import ripser
+from persim import plot_diagrams
+
+points = pd.read_csv("data_B.csv") 
+
+diagrams = ripser(points.values)['dgms']
+
+plt.figure(figsize=(6, 6))
+plt.scatter(points["x"], points["y"], s=10)
+plt.title("Point Cloud for data_B.csv")
+plt.axis("equal")
+plt.grid(True)
+plt.savefig("data_B_pointcloud.png", dpi=300)
+plt.show()
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+plot_diagrams(diagrams[0], ax=ax[0], show=False)
+plot_diagrams(diagrams[1], ax=ax[1], show=False)
+ax[0].set_title("H₀ Persistence Diagram (Connected Components)")
+ax[1].set_title("H₁ Persistence Diagram (1D Holes/Loops)")
+plt.tight_layout()
+plt.savefig("data_B_persistence_diagram.png", dpi=300)
+plt.show()
